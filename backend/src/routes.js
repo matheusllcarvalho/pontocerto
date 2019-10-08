210ca6f7c2a7ab59eda30b863b203b6269ce9224
Matheus Carvalho <matheusllcarvalho@gmail.com>
2019-10-07 22:24:31 -0300

    teste commit 1

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World 2' });
+  return res.json({ message: 'Hello World 3' });
 });
 
 module.exports = routes;
